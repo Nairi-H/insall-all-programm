f0e8af03064f84f7eb6466b2817f68575e7b4916
Nairi-H <nairi.hovakimyan.gp@gmail.com>
2021-01-06 09:46:27 +0400

    install commit

---
+++ install_all_programms.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo apt-get install vim
+
+sudo apt-get install g++
+
+sudo apt-get install make
+
+sudo apt-get install git
+
+#zoom
+wget -O Downloads/zoom.deb https://zoom.us/client/latest/zoom_amd64.deb
+cd Downloads
+sudo dpkg -i zoom.deb
+
+sudo apt-get install kturtle
+
+#telegram
+sudo add-apt-repository ppa:atareao/telegram
+sudo apt update && sudo apt install telegram
+
+sudo apt-get install inkscape
+
+#by Nairi for Manvel dzadza
